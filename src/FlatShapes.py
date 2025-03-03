52caa9793806d323aafb68801108937452f4f78d
LNUitTutor <lnu.it.tutor@gmail.com>
2025-03-03 09:12:39 +0200

    Update FlatShapes.py
    test part added

--- src/FlatShapes.py
+++ src/FlatShapes.py
@@ -2,7 +2,7 @@
 # Прямокутник уміє повідомляти свої площу та периметр
 
 class Rectangle:
-    def __init__(self,width,length):
+    def __init__(self,width=3,length=4):
         self.a=width
         self.b=length
     def __str__(self):
@@ -11,3 +11,14 @@ class Rectangle:
         return self.a*self.b
     def perim(self):
         return 2*(self.a+self.b)
+
+if __name__ == '__main__':
+    R = Rectangle(2,3)
+    print(R)
+    print('S =', R.area())
+    print('P =', R.perim())
+    P = Rectangle()
+    print(P)
+    print('S =', P.area())
+    print('P =', P.perim())
+
