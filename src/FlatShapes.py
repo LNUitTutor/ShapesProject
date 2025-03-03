acc1705654614e92c750b9595040e73056fd992a
LNUitTutor <lnu.it.tutor@gmail.com>
2025-03-03 09:57:47 +0200

    Abstract Shape added

--- src/FlatShapes.py
+++ src/FlatShapes.py
@@ -1,8 +1,22 @@
+import math
+from abc import ABC, abstractmethod
+
+# Абстрактний клас пов'язує всі типи плоских фігур
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    @abstractmethod
+    def perim(self):
+        pass
+    def __gt__(self, other):
+        return self.area() > other.area()
+
 # Оголосити клас, що моделює геометричну фігуру прямокутник
 # Прямокутник уміє повідомляти свої площу та периметр
 # Прямокутники порівнюють за площею
 
-class Rectangle:
+class Rectangle(Shape):
     def __init__(self,width=3,length=4):
         self.a=width
         self.b=length
@@ -12,8 +26,6 @@ class Rectangle:
         return self.a*self.b
     def perim(self):
         return 2*(self.a+self.b)
-    def __gt__(self, other):
-        return self.area() > other.area()
 
 # Оголосіть клас, що моделює квадрат
 
@@ -22,6 +34,19 @@ class Square(Rectangle):
         Rectangle.__init__(self, a, a)
     def __str__(self):
         return f'square:{self.a}x{self.a}'
+
+# Оголосіть класи, що моделюють круг і трикутник
+# Об'єднайте всі класи в одну ієрархію
+
+class Circle(Shape):
+    def __init__(self,radius=2):
+        self.r=radius
+    def __str__(self):
+        return f'circle:{self.r}'
+    def area(self):
+        return math.pi * self.r**2
+    def perim(self):
+        return 2 * math.pi * self.r 
 
 
 
@@ -38,5 +63,9 @@ if __name__ == '__main__':
     print(S)
     print('S =', S.area())
     print('P =', S.perim())
-    print(max(P,R,S))
+    C = Circle(3)
+    print(C)
+    print('S =', C.area())
+    print('P =', C.perim())
+    print(max(P,R,S,C))
 
