03dc0e576365080cb5e1ecb9a5a30f81b63e8c2e
LNUitTutor <lnu.it.tutor@gmail.com>
2025-03-10 09:36:15 +0200

    Cylinder extended

--- src/VolShapes.py
+++ src/VolShapes.py
@@ -6,9 +6,21 @@ class Cylinder:
         self.height = height
     def __str__(self):
         return f'Cylinder[r = {self.base.r}, h = {self.height}]'
+    def Volume(self):
+        return self.base.area()*self.height
+    def LateralArea(self):
+        return self.base.perim()*self.height
+    def BaseArea(self):
+        return self.base.area()
+    def TotalArea(self):
+        return 2*self.BaseArea()+self.LateralArea()
+        
 
 if __name__ == '__main__':
     a = Cylinder()
     b = Cylinder(5, 8)
     print(a)
+    print('v=', a.Volume(),'s=', a.TotalArea())
     print(b)
+    print('v=', b.Volume(),'s=', b.TotalArea())
+    
