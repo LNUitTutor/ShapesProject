30b2d2e03025b18a65e846e38de6d7623c3a7c66
LNUitTutor <lnu.it.tutor@gmail.com>
2025-03-12 19:14:18 +0200

    Abstract base class created
    Shape3D holds all general functionality

--- src/VolShapes.py
+++ src/VolShapes.py
@@ -1,33 +1,40 @@
-from FlatShapes import Circle
+from abc import ABC, abstractmethod
+from FlatShapes import *
 
-class Cylinder:
-    def __init__(self, radius = 1, height = 1):
-        self.base = Circle(radius)
-        self.height = height
-    def __str__(self):
-        return f'Cylinder[r = {self.base.r}, h = {self.height}]'
+class Shape3D(ABC):
+    def __init__(self, shape, number):
+        self.base = shape
+        self.height = number
     def Volume(self):
-        return self.base.area()*self.height
+        return self.base.area() * self.height
     def LateralArea(self):
-        return self.base.perim()*self.height
+        return self.base.perim() * self.height
     def BaseArea(self):
         return self.base.area()
     def TotalArea(self):
-        return 2*self.BaseArea()+self.LateralArea()
+        return 2*self.BaseArea() + self.LateralArea()
     def __gt__(self,other):
-        return self.Volume()> other.Volume()
-    
+        return self.Volume() > other.Volume()
+    @abstractmethod
+    def __str__(self):
+        pass
 
+class Cylinder(Shape3D):
+    def __init__(self, radius = 1, height = 1):
+        Shape3D.__init__(self, Circle(radius), height)
+    def __str__(self):
+        return f'Cylinder[r = {self.base.r}, h = {self.height}]'
+        
 if __name__ == '__main__':
     a = Cylinder()
     b = Cylinder(5, 8)
     print(a)
-    print('v=', a.Volume(),'s=', a.TotalArea())
+    print('v =', a.Volume(),'  s =', a.TotalArea())
     print(b)
-    print('v=', b.Volume(),'s=', b.TotalArea())
+    print('v =', b.Volume(),'  s =', b.TotalArea())
     if a>b:
-        print("max=",a)
+        print("max =",a)
     else:
-        print("max=",b)
+        print("max =",b)
         
     
